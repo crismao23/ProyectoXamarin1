20da5b53a575508b26b04b438e32154960669751
Crhistian Arenas <cmarenas@utp.edu.co>
2022-11-07 16:00:31 -0500

    Agregar archivos de proyecto.

---
+++ Unidad 2/Unidad 2/DataBase/DataBaseQuery.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using Unidad_2.Models;
+
+namespace Unidad_2.DataBase
+{
+    public class DataBaseQuery
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public DataBaseQuery(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<UserModel>().Wait();
+        }
+
+        #region CRUD
+
+        public Task<int> SaveUserModelAsync(UserModel  user)
+        {
+            return _database.InsertAsync(user);
+        }
+
+        public Task<List<UserModel>> GetUserModel()
+        {
+            return _database.Table<UserModel>().ToListAsync();
+        }
+
+        public Task<List<UserModel>> QueryUserModel(string query)
+        {
+            return _database.QueryAsync<UserModel>(query);
+        }
+
+
+
+
+
+        #endregion
+
+
+    }
+}
